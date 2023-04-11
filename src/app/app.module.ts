62e79b60bd178b2c1c46fbfcf7a39a02f519ddf9
Carlos Eduardo <ceinvestimentosllc@gmail.com>
2023-04-10 21:01:35 -0300

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { MainComponent } from './componentes/main/main.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
